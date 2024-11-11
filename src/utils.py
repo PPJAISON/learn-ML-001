955a9413dfdd17b6f5f3d1523be09e540a444e62
JaisonPappachan <pjaisonp@gmail.com>
2024-11-11 06:12:32 +0530

    hyper Params

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,11 @@ import dill
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+
+
 from src.logger import logging
+
 
 def save_object(file_path, obj):
     try:
@@ -23,7 +27,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models (X_train, y_train, X_test, y_test, models):
+def evaluate_models (X_train, y_train, X_test, y_test, models, param):
     try:
         report={}
         for i in range(len(list(models))):
@@ -31,6 +35,14 @@ def evaluate_models (X_train, y_train, X_test, y_test, models):
             model = list(models.values())[i]
 
             print(model)
+
+            param1= param[list(models.keys())[i]]
+            #gs = GridSearchCV(model, param1, cv=cv, n_jobs=n_jobs, verbose=verbose, refit=refit)
+            gs = GridSearchCV(model, param1, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            
 
             model.fit(X_train, y_train) # Train model
 
